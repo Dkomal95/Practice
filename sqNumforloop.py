8eb9473dfe7d02e69219d0c5784b6812d554f2f3
Komal Dasari <dkomal1295@gmail.com>
2023-01-31 11:55:03 +0530

    Add files via upload

---
+++ sqNumforloop.py
@@ -0,0 +1,33 @@
+# Square of numbers using for loop
+'''x=1
+for x in range(1,11):
+    print("Square of",x, "is", x*x)'''
+    
+    
+# print even numbers using for loop
+'''x=0
+for x in range(0,25):
+    if x%2==0:
+        print(x)'''
+        
+#for loop to print multiplicative table
+'''n=int(input("Enter a number: "))
+for i in range(1,11):
+    print(n*i)'''
+    
+#reverse count with 2 step difference
+'''for x in range(50,1,-2):
+    print(x)'''
+    
+#practice purpose
+s="KOMAL"
+print(s[1:3])
+print(s[1::-1])
+print(s[::2])
+print(s[0:])
+print(s[1:4:-1])
+print(s[:5:-2])
+print(s[::-1])
+print(s[-2])
+print(s[-1:-6:-2])
+
