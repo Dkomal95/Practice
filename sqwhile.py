d3390756071d98a7efe98500739347b56fc91a86
Komal Dasari <dkomal1295@gmail.com>
2023-01-31 11:55:26 +0530

    Add files via upload

---
+++ sqwhile.py
@@ -0,0 +1,6 @@
+ch='Y'
+while ch.lower()!="n":
+    n = int(input("Enter a no. to find the square : "))
+    print("The square of ",n,"is",n*n)
+    ch=str(input("Do you want to continue finding the square? Press 'Y' to continue and 'N' to stop. :"))
+print("End of the Program")
