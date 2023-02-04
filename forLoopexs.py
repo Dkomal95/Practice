477611b48e3039cfe185e702dcf822013093c0f7
Komal Dasari <dkomal1295@gmail.com>
2023-02-04 16:21:24 +0530

    Add files via upload

---
+++ forLoopexs.py
@@ -0,0 +1,22 @@
+'''list=['apple', 'orange', 'pineapple', 'guava', 'mango']
+for x in list:
+    print(x)'''
+    
+'''Name = 'KOMAL'
+for x in Name:
+    print(x)'''
+    
+'''for x in range(1,6):
+    for y in range(1,6):
+        print(y,end=' ')
+    print()'''
+        
+'''for i in range(1,6):
+    for j in range(1,6):
+        print("*",end=' ')
+    print()'''
+    
+for x in range(5):
+    if(x==3):
+        break
+    print("Number is ",x)
