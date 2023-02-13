fbc8bbe0286f62a36427572a06185cb76e929b2a
Komal Dasari <dkomal1295@gmail.com>
2023-02-13 08:29:41 +0530

    Add files via upload

---
+++ SwitchEX.py
@@ -0,0 +1,23 @@
+print('Welcome to Scaler Academy')
+print('1. visit python hub')
+print('2. visit dsa hub')
+print('3. visit java hub')
+print('4. more')
+
+print('input the suitable option in the next line:')
+textInput = int(input())
+
+if(textInput == 1):
+    print('Thank you. We are redirecting to python hub.')
+
+elif(textInput == 2):
+    print('Thank you. We are redirecting to dsa hub.')
+
+elif(textInput == 3):
+    print('Thank you. We are redirecting to java hub.')
+
+elif(textInput == 4):
+    print('Thank you. We are redirecting to more section.')
+else:
+    print('wrong input')
+
